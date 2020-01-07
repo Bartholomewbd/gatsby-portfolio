c830094922856f9a07f697359dd5b6eaf8a6a930
Brian Bartholomew <bartholomewbd@gmail.com>
2020-01-07 05:00:53 -0500

    added the projects to the project page and did the initial styling for it

--- src/components/project-card/project-card.js
+++ src/components/project-card/project-card.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import projectStyles from './project-card.module.scss'
+
 const ProjectCard = props => {
     return (
-        <div>
+        <div className={projectStyles.project}>
             <a href={props.liveUrl}>
-                <img src={props.projectImage}/>
+                <img src={props.projectImage} alt={props.imageAlt}/>
             </a>
             <a href={props.liveUrl}>
-                <button ><i className="fas fa-eye"></i>Project</button>
+                <button className="btn-light"><i className="fas fa-eye"></i> Project</button>
             </a>
             <a href={props.githubUrl}>
-                <button><i className="fab fa-github"></i>Github</button>
+                <button className="btn-dark"><i className="fab fa-github"></i> Github</button>
             </a>
             <p>{props.projectDescription}</p>
         </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
 
+import projectStyles from '../styles/project.module.scss'
+
 import ProjectCard from '../components/project-card/project-card'
+import happyHandsImage from '../img/projects/happyhands.png'
+import backgroundGeneratorImage from '../img/projects/backgroundgenerator.jpg'
+import faceDetectionImage from '../img/projects/facedetection.jpg'
+import kittySocialImage from '../img/projects/kittysocial.jpg'
 
 const Projects = () => {
     return (
         <Layout>
-            <ProjectCard liveUrl="https://happyhandsprep.com" githubUrl="https://github.com/Bartholomewbd/hhp" />
+            <div className={projectStyles.project}>
+                <h1>My <span className="text-secondary">Projects</span></h1>
+                <div className={projectStyles.projects}>
+                    <ProjectCard projectImage={happyHandsImage} liveUrl="https://happyhandsprep.com" githubUrl="https://github.com/Bartholomewbd/hhp" />
+                    <ProjectCard projectImage={backgroundGeneratorImage} liveUrl="https://bartholomewbd.github.io/bggradient/" githubUrl="https://github.com/Bartholomewbd/bggradient" />
+                    <ProjectCard projectImage={faceDetectionImage} liveUrl="https://bartholomewbd.github.io/imgdetection/" githubUrl="https://github.com/Bartholomewbd/imgdetection" />
+                    <ProjectCard projectImage={kittySocialImage} liveUrl="https://bartholomewbd.github.io/kittysocial/" githubUrl="https://github.com/Bartholomewbd/kittysocial" />
+                </div>
+            </div>
         </Layout>
     )
 }
